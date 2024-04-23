eaeaa1b9a2fea5db7a1ed1121981dd5e97cae793
Reese <charisearter.dev@gmail.com>
2024-04-23 10:41:47 -0400

    added api random functionality

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-console.log('JS is working');
+// Manipulate DOM to populate results
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuote = document.getElementById('new-quote');
+
+let apiQuotes = [];
+
+// Get Quotes from API
+async function getQuotes() {
+	const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+
+	//  Show new quote
+	function newQuote() {
+		// get random quote
+		const quote =
+			apiQuotes[Math.floor(Math.random() * Math.floor(apiQuotes.length))];
+		console.log(quote);
+	}
+
+	try {
+		const res = await fetch(apiUrl);
+		apiQuotes = await res.json();
+		newQuote();
+	} catch (error) {
+		// Catch error here
+	}
+}
+
+// on load
+getQuotes();

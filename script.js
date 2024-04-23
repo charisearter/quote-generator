f9fe381b873ed49a90128c59d394664b1d33baf7
Reese <charisearter.dev@gmail.com>
2024-04-23 11:40:17 -0400

    fixed newQuote fn issue and added tweet functionality

--- script.js
+++ script.js
@@ -7,18 +7,32 @@ const newQuoteBtn = document.getElementById('new-quote');
 
 let apiQuotes = [];
 
+//  Show new quote
+function newQuote() {
+	// get random quote
+	const quote =
+		apiQuotes[Math.floor(Math.random() * Math.floor(apiQuotes.length))];
+	// Populate UI with res data
+	// Check if there is an author, if not replace with Unknown
+	if (!quote.author) {
+		authorText.textContent = 'Unknown';
+	} else {
+		authorText.textContent = quote.author;
+	}
+
+	//  Check quote length to determine styling
+	if (quote.text.length > 120) {
+		quoteText.classList.add('long-quote');
+	} else {
+		quoteText.classList.remove('long-quote');
+	}
+	quoteText.textContent = quote.text;
+}
 // Get Quotes from API
 async function getQuotes() {
 	const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
 
-	//  Show new quote
-	function newQuote() {
-		// get random quote
-		const quote =
-			apiQuotes[Math.floor(Math.random() * Math.floor(apiQuotes.length))];
-		console.log(quote);
-	}
-
+	// Try to Get API data
 	try {
 		const res = await fetch(apiUrl);
 		apiQuotes = await res.json();
@@ -27,6 +41,16 @@ async function getQuotes() {
 		// Catch error here
 	}
 }
+
+// Tweet Quote
+function tweetQuote() {
+	const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+	// open twitter window in a new tab
+	window.open(twitterUrl, '_blank');
+}
+// Event Listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 
 // on load
 getQuotes();
